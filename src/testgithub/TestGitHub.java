e648d46377003afaadfa8f9372a398ee426f4f0d
ArtjomKozmik <artjom.kozmik@gmail.com>
2013-10-22 13:30:18 +0200

    3-October main project 11:14

---
+++ src/testgithub/TestGitHub.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgithub;
+
+/**
+ *
+ * @author Artjom
+ */
+public class TestGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
